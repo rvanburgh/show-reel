625aa819f4c20826c98d717f52ece5d2a1c0fa9f
Roger Vanburgh <roger@Admins-MacBook-Pro.local>
2024-08-14 11:34:22 +0100

    Added styling, bio

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Amber Ahmad</h1>
+        <h1 style={{fontFamily: 'inherit'}}>Amber Ahmad</h1>
+        <h2 style={{fontFamily: 'inherit'}}> Journalist/Producer</h2>
       </header>
       <main className="App-main">
         <div className="video-container">
@@ -16,6 +17,24 @@ function App() {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
+        </div>
+        <div style={{margin:'8px', backgroundColor: 'white', borderRadius: '4px', background: '#6a7c96', padding:'16px'}}>
+          <p style={{fontSize: '20px', color: 'white', lineHeight:'28px'}}>
+          I'm a journalist and producer specialising in TV broadcast, video documentary and social media production. 
+          I'm also a creative copywriter and digital journalist with expeirence writing web journalism and brand storytelling. 
+          My work has featured in broadcast outlets including ITV News, Euronews, Channel 5, cultural magazine titles and household brands.
+          </p>
+          
+          <p style={{fontSize: '20px', color: 'white', lineHeight:'28px'}}>
+            </p>
+        </div>
+        <div style={{backgroundColor: 'white', borderRadius: '4px', background: '#6a7c96', padding:'16px'}}>
+          <p style={{fontSize: '20px', color: 'white', lineHeight:'28px'}}>
+          For freelance enquiries contact: <a style={{color: 'inherit'}} href="mailto:ambereveahmad@gmail.com">ambereveahmad@gmail.com</a> / <a style={{color: 'inherit'}} href="tel:+44 7858618261">+44 7858618261</a>
+          </p>
+          
+          <p style={{fontSize: '20px', color: 'white', lineHeight:'28px'}}>
+            </p>
         </div>
       </main>
     </div>
